eb5e656443dcc0fc2451136cf194caa821a1771e
venetta999 <66510220+venetta999@users.noreply.github.com>
2021-08-30 04:19:09 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,6 +28,6 @@ printf "\nSetup Completed " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - NAHID -c """$CRP"""
+su - venecore -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n'
 printf 'Your SUDO Pasword is: 123456 \n'
